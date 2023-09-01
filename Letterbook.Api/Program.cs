02dab5f79c3d037871c7d33b46b148cdfd563548
Jenniferplusplus <contact@jenniferplusplus.com>
2023-09-01 14:42:27 -0500

    Setup OpenIdDict server

--- Letterbook.Api/Program.cs
+++ Letterbook.Api/Program.cs
@@ -19,6 +19,43 @@ public class Program
             options.Conventions.Add(new RouteTokenTransformerConvention(new SnakeCaseRouteTransformer()));
         });
         
+        // Register OIDC
+        builder.Services.AddOpenIddict()
+
+            // Register the OpenIddict core components.
+            .AddCore(options =>
+            {
+                // Configure OpenIddict to use the Entity Framework Core stores and models.
+                // Note: call ReplaceDefaultEntities() to replace the default entities.
+                options.UseEntityFrameworkCore()
+                    .UseDbContext<TransactionalContext>();
+            })
+            .AddServer(options =>
+            {
+                // Enable the token endpoint.
+                options.SetTokenEndpointUris("connect/token");
+
+                // Enable the client credentials flow.
+                options.AllowClientCredentialsFlow();
+                options.AllowAuthorizationCodeFlow();
+
+                // Register the signing and encryption credentials.
+                options.AddDevelopmentEncryptionCertificate()
+                    .AddDevelopmentSigningCertificate();
+
+                // Register the ASP.NET Core host and configure the ASP.NET Core options.
+                options.UseAspNetCore()
+                    .EnableTokenEndpointPassthrough();
+            })
+            .AddValidation(options =>
+            {
+                // Import the configuration from the local OpenIddict server instance.
+                options.UseLocalServer();
+
+                // Register the ASP.NET Core host.
+                options.UseAspNetCore();
+            });
+        
         // Register config
         var coreOptions = builder.Configuration.GetSection(CoreOptions.ConfigKey);
         builder.Services.Configure<CoreOptions>(coreOptions);
@@ -36,6 +73,17 @@ public class Program
         builder.Services.AddScoped<IActivityAdapter, ActivityAdapter>();
         builder.Services.AddSingleton<IMessageBusAdapter, RxMessageBus>();
         builder.Services.AddDbContext<TransactionalContext>();
+        
+        // TODO: Move to db adapter
+        // services.AddDbContext<ApplicationDbContext>(options =>
+        // {
+        //     // Configure Entity Framework Core to use Microsoft SQL Server.
+        //     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        //
+        //     // Register the entity sets needed by OpenIddict.
+        //     // Note: use the generic overload if you need to replace the default OpenIddict entities.
+        //     options.UseOpenIddict();
+        // });
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
@@ -55,6 +103,7 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
         app.UseAuthorization();
         
         app.UsePathBase(new PathString("/api/v1"));
